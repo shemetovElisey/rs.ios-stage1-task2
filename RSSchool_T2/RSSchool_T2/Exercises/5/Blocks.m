ffaf8063c051f11659d33e475787797dd8d89bbc
Elisey <shemetov.elisey@gmail.com>
2020-03-28 01:05:59 +0700

    fix: block

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,9 +1,15 @@
 #import "Blocks.h"
 
 @implementation Blocks
-@property (nonatomic) BlockA blockA;
-@property (nonatomic) BlockB blockB;
-@property (nonatomic) BlockC blockC;
+-(instancetype)init {
+    self = [super init];
+    if (self){
+        _blockA = ^(NSArray *array){};
+        _blockB = ^(Class class){};
+        _blockC = ^(NSObject *result){};
+    }
+    return self;
+}
 
 @end
 
